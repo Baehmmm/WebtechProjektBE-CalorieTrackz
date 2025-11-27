5a8484ba819006d33fff8b6ca7b9b224f9ca3f96
Gold 1 Hardstuck <Jim-Philip.Sedlacek@Student.HTW-Berlin.de>
2025-11-27 15:36:48 +0100

    getter und setter wieder hinzugefügt

--- src/main/java/htw/webtech/CalorieTrackz/FoodEntry.java
+++ src/main/java/htw/webtech/CalorieTrackz/FoodEntry.java
@@ -11,6 +11,54 @@ public class FoodEntry {
      * Lebensmittel Eingang
      */
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public double getProtein() {
+        return protein;
+    }
+
+    public void setProtein(double protein) {
+        this.protein = protein;
+    }
+
+    public double getCarbohydrates() {
+        return carbohydrates;
+    }
+
+    public void setCarbohydrates(double carbohydrates) {
+        this.carbohydrates = carbohydrates;
+    }
+
+    public double getFat() {
+        return fat;
+    }
+
+    public void setFat(double fat) {
+        this.fat = fat;
+    }
+
     public FoodEntry() {
     }
 
